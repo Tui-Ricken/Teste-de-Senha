979d25b9cda96effd2eda9dff0edb5b4fc041ec1
Tui-Ricken <arthurricken1@gmail.com>
2026-04-16 20:58:04 -0300

    feat: adicionar senha para passar nos testes

--- Senha/src/utils/constants.js
+++ Senha/src/utils/constants.js
@@ -1,6 +1,7 @@
 const constantes = {
     'SENHA_MENOR_8_CARACTERES':'senha',
     'SENHA_MAIOR_OU_IGUAL_8_CARACTERES':'senha123',
+    'SENHA_CORRETA':'Senha123@',
 }
 
 module.exports = {constantes};
