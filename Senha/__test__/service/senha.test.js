b2dc2c5abfc9c918e3f72abe21e280c498a4e74f
Tui-Ricken <arthurricken1@gmail.com>
2026-04-16 20:46:28 -0300

    feat: deve reijatar a senha com menos de 8 caractere

--- Senha/__test__/service/senha.test.js
+++ Senha/__test__/service/senha.test.js
@@ -1,11 +1,12 @@
 const {Senha} = require("../../src/model/Senha");
 const {SenhaService} = require("../../src/service/SenhaService");
-const casos = require("../service/dados/senha.json");
+// const casos = require("../service/dados/senha.json");
+const {constantes} = require("../../src/utils/constants");
 
 describe("Senha",()=>{
     test("Testa tamanho da senha (senha menor que o minimo)", () => {
         // Arrange
-        const senha = constantes.SENHA_MENOR_8_CARACTERES;
+        const senha = new Senha({senha:constantes.SENHA_MENOR_8_CARACTERES});
 
         // Act
         const saida = SenhaService.ValidarTamanho(senha)
