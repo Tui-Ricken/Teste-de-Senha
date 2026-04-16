4c8a6f9e8e63678352670f061b33cd0506699121
Tui-Ricken <arthurricken1@gmail.com>
2026-04-16 20:33:21 -0300

    test: deve rejeitar senha com menos de 8 caracteres

---
+++ Senha/__test__/service/senha.test.js
@@ -0,0 +1,16 @@
+const {Senha} = require("../../src/model/Senha");
+const {SenhaService} = require("../../src/service/SenhaService");
+const casos = require("../service/dados/senha.json");
+
+describe("Senha",()=>{
+    test("Testa tamanho da senha (senha menor que o minimo)", () => {
+        // Arrange
+        const senha = constantes.SENHA_MENOR_8_CARACTERES;
+
+        // Act
+        const saida = SenhaService.ValidarTamanho(senha)
+
+        // Assert
+        expect(false).toBe(saida);
+    });
+})
