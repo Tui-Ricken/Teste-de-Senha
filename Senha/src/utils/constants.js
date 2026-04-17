d9e8ebf8e445a34574d38fad00e80f418c9d90d7
Tui-Ricken <arthurricken1@gmail.com>
2026-04-17 20:38:46 -0300

    feat: adicionar constante com espaço

--- Senha/src/utils/constants.js
+++ Senha/src/utils/constants.js
@@ -3,6 +3,7 @@ const constantes = {
     'SENHA_MAIOR_OU_IGUAL_8_CARACTERES':'senha123',
     'SENHA_CORRETA':'Senha123@',
     'SENHA_MAIUSCULA':'SENHA123',
+    'SENHA_ESPAÇO' : 'Senha 123',
 }
 
 module.exports = {constantes};
