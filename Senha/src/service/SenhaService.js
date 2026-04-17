1a5f837217697543b973f8d4d3eb1cff3e7f3049
Tui-Ricken <arthurricken1@gmail.com>
2026-04-17 19:50:24 -0300

    feat: implementaçao se tem letra minuscula na senha

--- Senha/src/service/SenhaService.js
+++ Senha/src/service/SenhaService.js
@@ -3,9 +3,13 @@ class SenhaService{
     static ValidarTamanho(senha){
         if(senha.senha.length>=8) return true;
     }
-    
+
     static validarLetraMaiuscula(senha){
         if(/[A-Z]/.test(senha.senha)) return true;
+    }
+
+    static validarLetraMinuscula(senha){
+        if((/[a-z]/).test(senha.senha)) return true;
     }
 }
 module.exports = {SenhaService};
