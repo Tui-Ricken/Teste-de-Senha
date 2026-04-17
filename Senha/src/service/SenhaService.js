ba51bc78d7e7176612da62190b04ba566f6572b0
Tui-Ricken <arthurricken1@gmail.com>
2026-04-17 19:41:18 -0300

    feat: funçao para validar se tem letra maiuscula

--- Senha/src/service/SenhaService.js
+++ Senha/src/service/SenhaService.js
@@ -3,6 +3,7 @@ class SenhaService{
     static ValidarTamanho(senha){
         if(senha.senha.length>=8) return true;
     }
+    
     static validarLetraMaiuscula(senha){
         if(/[A-Z]/.test(senha.senha)) return true;
     }
