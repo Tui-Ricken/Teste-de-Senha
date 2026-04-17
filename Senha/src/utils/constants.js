db13d2f3c0ad5efc4a1f4544b7d3f8dca93286a1
Tui-Ricken <arthurricken1@gmail.com>
2026-04-17 20:20:07 -0300

    feat: adicionar constante de senha com letras maiuculas

--- Senha/src/utils/constants.js
+++ Senha/src/utils/constants.js
@@ -2,6 +2,7 @@ const constantes = {
     'SENHA_MENOR_8_CARACTERES':'senha',
     'SENHA_MAIOR_OU_IGUAL_8_CARACTERES':'senha123',
     'SENHA_CORRETA':'Senha123@',
+    'SENHA_MAIUSCULA':'SENHA123',
 }
 
 module.exports = {constantes};
